676a3c833d3da0853030525a82f32f93899f04d2
Ricardo Bustamante <ricardo.bustamante@tapps.com.br>
2019-04-17 13:51:22 -0300

    Add files.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private static readonly int Moving = Animator.StringToHash("Moving");
+    private static readonly int Jumping = Animator.StringToHash("Jumping");
+    private static readonly int VerticalVelocity = Animator.StringToHash("VerticalVelocity");
+
+
+    private Rigidbody2D rigidBody;
+
+    private SpriteRenderer spriteRenderer;
+
+    private Animator animator;
+
+    public float acceleration = 5;
+    public float maxVelocity = 5;
+
+    public float jumpSpeed = 5;
+
+    private Vector2 playerInput;
+
+    public bool grounded;
+    public float timeSinceJump = 0;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        playerInput.x = Input.GetAxisRaw("Horizontal");
+        playerInput.y = Input.GetAxisRaw("Vertical");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        MovementLogic();
+    }
+
+    private void MovementLogic()
+    {
+        var hForce = playerInput.x * acceleration;
+
+        if (!grounded)
+        {
+            timeSinceJump += Time.fixedDeltaTime;
+        }
+
+        if (Mathf.Approximately(hForce, 0))
+        {
+            hForce = -rigidBody.velocity.x;
+        }
+
+        rigidBody.AddForce(new Vector2(hForce, 0), ForceMode2D.Impulse);
+
+        if (!Mathf.Approximately(playerInput.x, 0))
+        {
+            spriteRenderer.flipX = playerInput.x < 0;
+        }
+
+        var vel = rigidBody.velocity;
+
+        if (grounded && playerInput.y > 0)
+        {
+            vel.y = jumpSpeed;
+            grounded = false;
+        }
+
+        vel.x = Mathf.Clamp(vel.x, -maxVelocity, maxVelocity);
+        rigidBody.velocity = vel;
+        
+        animator.SetBool(Moving, Mathf.Abs(vel.x) > 0.1f);
+        animator.SetBool(Jumping, !grounded);
+        animator.SetFloat(VerticalVelocity, vel.y);
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (timeSinceJump > 0.2f && !grounded && other.gameObject.CompareTag("Ground"))
+        {
+            grounded = true;
+            timeSinceJump = 0;
+        }
+    }
+}
