7354b6230e4ff11aca2a0193c77c31badc3f7da1
Ricardo Bustamante <ricardobqueiroz@gmail.com>
2019-04-22 08:41:10 -0300

    Fix camera aspect ratio. Add scenes to project settings.

---
+++ Assets/Scripts/CameraAspectFixer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CameraAspectFixer : MonoBehaviour {
+    public Camera Camera;
+
+    public float TargetAspect = 16f / 9f;
+
+    private float originalSize;
+
+    private void Start() {
+        originalSize = Camera.orthographicSize;
+        CalculateAspectRatio();
+    }
+
+    private void CalculateAspectRatio() {
+        var screenRatio = Screen.width / (float) Screen.height;
+        if (Screen.width / (float) Screen.height < TargetAspect) {
+            Camera.orthographicSize = originalSize * TargetAspect / screenRatio;
+        } else {
+            Camera.orthographicSize = originalSize;
+        }
+    }
+}
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,24 +1,30 @@
-namespace Game
-{
+using TMPro;
+
+namespace Game {
     using System;
     using System.Collections.Generic;
     using UnityEngine;
 
-    public class GameController : MonoBehaviour
-    {
+    public class GameController : MonoBehaviour {
+        public TextMeshProUGUI FruitText;
+        public TextMeshProUGUI FruitTextShadow;
+
+        public TextMeshProUGUI GemText;
+        public TextMeshProUGUI GemTextShadow;
+
         private static GameController instance;
 
         private static Dictionary<CollectibleItem.CollectibleTypes, int> totalItemCount;
         private static Dictionary<CollectibleItem.CollectibleTypes, int> itemGetCount;
 
+        private static Dictionary<CollectibleItem.CollectibleTypes, TextMeshProUGUI> Text;
+        private static Dictionary<CollectibleItem.CollectibleTypes, TextMeshProUGUI> Shadow;
+
         private bool paused;
-        
-        public static GameController Instance
-        {
-            get
-            {
-                if (instance == null)
-                {
+
+        public static GameController Instance {
+            get {
+                if (instance == null) {
                     instance = FindObjectOfType<GameController>();
                 }
 
@@ -26,54 +32,72 @@ namespace Game
             }
         }
 
-        public void GetItem(CollectibleItem.CollectibleTypes itemType)
-        {
+        public void GetItem(CollectibleItem.CollectibleTypes itemType) {
             itemGetCount[itemType]++;
+            UpdateItemInformation(itemType);
+
             CheckVictoryCondition();
         }
 
-        private void CheckVictoryCondition()
-        {
+        private static void UpdateItemInformation(CollectibleItem.CollectibleTypes itemType) {
+            var text = $"{itemGetCount[itemType]}/{totalItemCount[itemType]}";
+            Text[itemType].text = text;
+            Shadow[itemType].text = text;
+        }
+
+        private void CheckVictoryCondition() {
             if (itemGetCount[CollectibleItem.CollectibleTypes.Cherry] ==
-                totalItemCount[CollectibleItem.CollectibleTypes.Cherry])
-            {
+                totalItemCount[CollectibleItem.CollectibleTypes.Cherry]) {
                 Debug.Log("WIN!");
             }
         }
 
-        public void Start()
-        {
+        public void Start() {
+            Text = new Dictionary<CollectibleItem.CollectibleTypes, TextMeshProUGUI>() {
+                {
+                    CollectibleItem.CollectibleTypes.Cherry, FruitText
+                }, {
+                    CollectibleItem.CollectibleTypes.Crystal, GemText
+                }
+            };
+
+            Shadow = new Dictionary<CollectibleItem.CollectibleTypes, TextMeshProUGUI>() {
+                {
+                    CollectibleItem.CollectibleTypes.Cherry, FruitTextShadow
+                }, {
+                    CollectibleItem.CollectibleTypes.Crystal, GemTextShadow
+                }
+            };
+
             totalItemCount = new Dictionary<CollectibleItem.CollectibleTypes, int>();
             itemGetCount = new Dictionary<CollectibleItem.CollectibleTypes, int>();
-            foreach (CollectibleItem.CollectibleTypes type in Enum.GetValues(typeof(CollectibleItem.CollectibleTypes)))
-            {
+            var itemTypes = Enum.GetValues(typeof(CollectibleItem.CollectibleTypes));
+            foreach (CollectibleItem.CollectibleTypes type in itemTypes) {
                 totalItemCount[type] = 0;
                 itemGetCount[type] = 0;
             }
 
             var items = FindObjectsOfType<CollectibleItem>();
-            foreach (var item in items)
-            {
+            foreach (var item in items) {
                 totalItemCount[item.ItemType]++;
+            }
+
+            foreach (CollectibleItem.CollectibleTypes type in itemTypes) {
+                UpdateItemInformation(type);
             }
         }
 
-        public void Update()
-        {
-            var pause = Input.GetButtonDown("Cancel");
-            if (pause)
-            {
+        public void Update() {
+            var esc = Input.GetButtonDown("Cancel");
+            var enter = Input.GetButtonDown("Submit");
+            if (esc || (enter && !paused)) {
                 paused = !paused;
-                
-                if (paused)
-                {
+
+                if (paused) {
                     Time.timeScale = 1;
-                }
-                else
-                {
+                } else {
                     Time.timeScale = 0;
                 }
-                
             }
         }
     }
